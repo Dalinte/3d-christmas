58a75c55336d2929ff5e00c2712ae52fcf25fa98
Dalinte <kollekcionerssd1@gmail.com>
2024-01-07 18:45:14 +0300

    add bottom reflection

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { MainScene } from './components/MainScene';
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
+    <Canvas shadows camera={{ position: [0, 2, 8], fov: 42 }}>
       <color attach="background" args={['#171720']} />
       <fog attach="fog" args={['#171720', 10, 30]} />
       <MainScene />
--- src/components/MainScene.tsx
+++ src/components/MainScene.tsx
@@ -1,6 +1,11 @@
-import { CameraControls, Environment, Text } from '@react-three/drei';
+import {
+  CameraControls,
+  Environment,
+  MeshReflectorMaterial,
+  Text,
+} from '@react-three/drei';
 import type { CameraControls as CameraControlsType } from '@react-three/drei';
-import { ChristmasModel } from './ChristmasScene';
+import { ChristmasModel } from './ChristmasModel';
 import { MathUtils } from 'three';
 import { useEffect, useRef } from 'react';
 
@@ -26,6 +31,7 @@ export const MainScene = () => {
         lineHeight={0.8}
         textAlign={'center'}
         rotation-y={MathUtils.degToRad(25)}
+        anchorY={'bottom'}
       >
         HAPPY {'\n'} NEW YEAR
         <meshBasicMaterial color={'white'} />
@@ -33,6 +39,24 @@ export const MainScene = () => {
       <group rotation-y={MathUtils.degToRad(-25)} position-x={3.1}>
         <ChristmasModel scale={1.4} />
       </group>
+      <mesh position-y={-0.48} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[100, 100]} />
+        <MeshReflectorMaterial
+          blur={[100, 100]}
+          resolution={2048}
+          mixBlur={1}
+          mixStrength={10}
+          roughness={1}
+          depthScale={1}
+          transparent
+          opacity={0.5}
+          minDepthThreshold={0.4}
+          maxDepthThreshold={1.4}
+          color="#333"
+          metalness={0.5}
+          mirror={1}
+        />
+      </mesh>
       <Environment preset={'sunset'} />
     </>
   );
