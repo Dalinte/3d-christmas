6f9c0f911d499cd15329899b5aa95b565243f643
Dalinte <kollekcionerssd1@gmail.com>
2024-01-07 19:41:45 +0300

    split components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { MainScene } from './components/MainScene';
+import { Canvas } from '@react-three/fiber';
 
 function App() {
-  return <MainScene />;
+  return (
+    <Canvas shadows camera={{ position: [0, 2, 8], fov: 42 }}>
+      <color attach="background" args={['#171720']} />
+      <fog attach="fog" args={['#171720', 10, 30]} />
+      <MainScene />
+    </Canvas>
+  );
 }
 
 export default App;
--- src/components/MainScene.tsx
+++ src/components/MainScene.tsx
@@ -5,25 +5,20 @@ import { NewYearText } from './NewYearText';
 import { ReflectorFloor } from './ReflectorFloor';
 import { Camera } from './Camera';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
-import { Canvas } from '@react-three/fiber';
 
 export const MainScene = () => {
   return (
     <>
-      <Canvas shadows camera={{ position: [0, 2, 8], fov: 42 }}>
-        <color attach="background" args={['#171720']} />
-        <fog attach="fog" args={['#171720', 10, 30]} />
-        <Camera />
-        <NewYearText />
-        <group rotation-y={MathUtils.degToRad(-25)} position-x={3.1}>
-          <ChristmasModel scale={1.4} />
-        </group>
-        <ReflectorFloor />
-        <Environment preset={'sunset'} />
-        <EffectComposer>
-          <Bloom mipmapBlur intensity={1} />
-        </EffectComposer>
-      </Canvas>
+      <Camera />
+      <NewYearText />
+      <group rotation-y={MathUtils.degToRad(-25)} position-x={3.1}>
+        <ChristmasModel scale={1.4} />
+      </group>
+      <ReflectorFloor />
+      <Environment preset={'sunset'} />
+      <EffectComposer>
+        <Bloom mipmapBlur intensity={1} />
+      </EffectComposer>
     </>
   );
 };
