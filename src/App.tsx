d8a8f2f99c1db522a1e7e2200982d68bc14f0c9b
Dalinte <kollekcionerssd1@gmail.com>
2024-01-07 14:29:56 +0300

    disabled custom properties on tags, fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Canvas } from "@react-three/fiber";
-import { MainScene } from "./components/MainScene";
+import { Canvas } from '@react-three/fiber';
+import { MainScene } from './components/MainScene';
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-      <color attach="background" args={["#171720"]} />
+      <color attach="background" args={['#171720']} />
       <MainScene />
     </Canvas>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import {createRoot} from 'react-dom/client'
-import App from './App.js'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.js';
+import './index.css';
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
-createRoot(root!).render(
-    <App />
-)
+createRoot(root!).render(<App />);
