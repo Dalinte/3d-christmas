34d9720227f0ecb60511e09a2300b7926abd3f45
Dalinte <kollekcionerssd1@gmail.com>
2024-01-07 15:37:03 +0300

    add camera animations

--- src/components/MainScene.tsx
+++ src/components/MainScene.tsx
@@ -1,11 +1,25 @@
-import { Environment, OrbitControls, Text } from '@react-three/drei';
+import { CameraControls, Environment, Text } from '@react-three/drei';
+import type { CameraControls as CameraControlsType } from '@react-three/drei';
 import { ChristmasModel } from './ChristmasScene';
 import { MathUtils } from 'three';
+import { useEffect, useRef } from 'react';
 
 export const MainScene = () => {
+  const controls = useRef<CameraControlsType>(null);
+
+  const intro = async () => {
+    controls.current?.dolly(-22);
+    controls.current!.smoothTime = 1.6;
+    controls.current?.dolly(22, true);
+  };
+
+  useEffect(() => {
+    intro();
+  }, []);
+
   return (
     <>
-      <OrbitControls />
+      <CameraControls ref={controls} />
       <Text
         font={'fonts/Poppins-Black.ttf'}
         position={[-1.3, -0.5, 1]}
